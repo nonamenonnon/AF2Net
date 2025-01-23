728937cd7416227a8e7fc3e3b3b1d61ab43f4c79
nonamenonnon <190715015+nonamenonnon@users.noreply.github.com>
2025-01-23 15:41:11 +0800

    Update options_cod.py

--- options_cod.py
+++ options_cod.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--epoch', type=int, default=170, help='epoch number')
+parser.add_argument('--epoch', type=int, default=200, help='epoch number')
 parser.add_argument('--lr', type=float, default=5e-5, help='learning rate')  # 5e-5
 parser.add_argument('--batchsize', type=int, default=16, help='training batch size')
 parser.add_argument('--trainsize', type=int, default=416, help='training dataset size')
@@ -13,5 +13,5 @@ parser.add_argument('--rgb_root', type=str, default='./TrainDataset/Imgs/', help
 parser.add_argument('--gt_root', type=str, default='./TrainDataset/GT/', help='the training gt images root')
 parser.add_argument('--test_rgb_root', type=str, default='./TestDataset/CAMO/Imgs/', help='the test gt images root')
 parser.add_argument('--test_gt_root', type=str, default='./TestDataset/CAMO/GT/', help='the test gt images root')
-parser.add_argument('--save_path', type=str, default='../autodl-tmp/xiaorong3/', help='the path to save models and logs')
+parser.add_argument('--save_path', type=str, default='../***************************/', help='the path to save models and logs')
 opt = parser.parse_args()
